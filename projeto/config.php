37e2fe7eb8d65be92f90e5729a8f88d8aa55208d
Leonardo-Rodrigues-Santana <70644760+Leonardo-Rodrigues-Santana@users.noreply.github.com>
2021-06-09 23:29:54 -0300

    Create config.php

---
+++ projeto/config.php
@@ -0,0 +1,10 @@
+<?php
+
+##arquivo que contem variaveis que serão usadas no arquivo de configuração do BD
+
+define('HOST', 'localhost');
+define('USER', 'postgres');
+define('PASS', '12345678');
+define('BASE', 'banco_vendas');
+
+?>
